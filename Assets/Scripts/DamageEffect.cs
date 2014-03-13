938d7a8fc5051d065b0498a614f444b8a8453735
Alexander Solan <alexander.solan@gmail.com>
2014-03-13 08:36:55 +0200

    Added some bitchin' SFX.

---
+++ Assets/Scripts/DamageEffect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageEffect : MonoBehaviour {
+
+	private float resize, selfDestructTimer;
+	private GameObject gc;
+	Color color;
+
+	void Awake () {
+		resize = 0;
+		selfDestructTimer = 0;
+		gc = GameObject.FindGameObjectWithTag("GameController");
+		GCScript Script1 = gc.GetComponent<GCScript>();
+		this.GetComponent<TextMesh>().text = Script1.qad.ToString();
+		color = this.renderer.material.color;
+	}
+
+
+	// Use this for initialization
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		resize += Time.deltaTime;
+		selfDestructTimer += Time.deltaTime;
+		this.GetComponent<TextMesh>().fontSize++;
+		// if (resize > 0.02f) {this.GetComponent<TextMesh>().fontSize++; resize = 0;}
+		if (selfDestructTimer > 0.6f) {color.a -= 0.05f; this.renderer.material.color = color;}
+		if (selfDestructTimer > 1) {Destroy(this.gameObject);}
+	
+	}
+}
--- Assets/Scripts/GCScript.cs
+++ Assets/Scripts/GCScript.cs
@@ -6,15 +6,17 @@ public class GCScript : MonoBehaviour {
 	private float autoticker = 0;
 	private GoTweenChain chain;
 	public int power, endurance, speed, joustbonus, playHP, experience, pLocX, pLocZ, oldLocX, oldLocZ, dLocX, dLocZ, level, movement, qad;
-	public int mapX, mapZ, killCount, spawnCount;
+	public int mapX, mapZ, killCount, spawnCount, monCount;
 	public GameObject powerNum, endNum, speedNum, jbonusNum, moveNum, HPNum, EXPNum, levelNum, killsNum, gameOverText, player, enemy, stabVictim;
+	public GameObject playerChargeHalo, playerChargeHaloSuper, playerChargeHaloThree;
 	private int[,] mapGrid, playerLoc;
 	private bool conflict = false;
 	private bool blinkUp = true;
+	private bool rotateCW = true;
 	private float countdown, cooldown, ticker;
 	public bool gameOver = false;
 	public int newMonX, newMonZ, ranSide, ranSquareX, ranSquareZ, ranType;
-	public Transform newMonType, goblin, ork, krusher;
+	public Transform newMonType, goblin, ork, krusher, damageEffect, gOT;
 	
 	// Use this for initialization
 	void Start () {
@@ -22,10 +24,10 @@ public class GCScript : MonoBehaviour {
 		pLocZ = 0;
 		movement = 2;
 		level = 0;
-		power = 1;
+		power = 5;
 		endurance = 0;
 		experience = 0;
-		playHP = 10;
+		playHP = 100;
 		speed = 2;
 		joustbonus = 0;
 		killCount = 0;
@@ -47,6 +49,14 @@ public class GCScript : MonoBehaviour {
 				gameOverText.GetComponent<TextMesh>().fontSize--;
 				if (gameOverText.GetComponent<TextMesh>().fontSize < 80) {blinkUp = true;}
 			}
+
+			if (rotateCW) {gOT.Rotate(0, 0, 100 * Time.deltaTime); countdown += Time.deltaTime;
+				if (countdown > 0.3f) {rotateCW = false; countdown = 0;}
+			}
+			else {gOT.Rotate(0, 0, -100 * Time.deltaTime); countdown += Time.deltaTime;
+				if (countdown > 0.3f) {rotateCW = true; countdown = 0;}
+			}
+
 		}
 
 		else {
@@ -54,6 +64,19 @@ public class GCScript : MonoBehaviour {
 			if (countdown > 0) {countdown -= Time.deltaTime;}
 			else 
 			{
+				// playerChargeHalo.transform.position = new Vector3(player.transform.position.x * 1.05f, 5.5f, player.transform.position.z * 1.05f);
+				playerChargeHalo.transform.position = player.transform.position * 1.05f;
+				playerChargeHalo.light.intensity = joustbonus;
+				if (joustbonus >= 8) {playerChargeHaloSuper.light.intensity = joustbonus - 8;}
+				else {playerChargeHaloSuper.light.intensity = 0;}
+				if (joustbonus >= 16) {playerChargeHaloThree.light.intensity = joustbonus - 16;}
+				else {playerChargeHaloThree.light.intensity = 0;}
+				if (joustbonus >= 25 && joustbonus < 50) {player.particleSystem.emissionRate = (joustbonus - 24) * 100;
+					player.particleSystem.startSpeed = 5 + 2 * (joustbonus - 24);}
+				else {player.particleSystem.emissionRate = 0;}
+				if (joustbonus >= 50) {player.particleSystem.Emit (4000); 
+					player.particleSystem.startSpeed = 5 + (joustbonus - 50);
+					player.particleSystem.emissionRate = 0;}
 				if (movement <= 0){movement = speed;}
 				else		 
 				{
@@ -119,6 +142,7 @@ public class GCScript : MonoBehaviour {
 	void MoveFunct () {
 		movement--;
 		joustbonus++;
+	
 		foreach(GameObject enemy in GameObject.FindGameObjectsWithTag("foe"))
 		{
 
@@ -151,19 +175,30 @@ public class GCScript : MonoBehaviour {
 			countdown = 0f;
 			if (movement <= 0) {MonSpawn ();}
 		}
+
+
 	}
 	void FightFunct () {
 
 		MonControl Script3 = stabVictim.GetComponent<MonControl>();
-		qad = 0;
-		if (Script3.monEnd <= power * joustbonus) {qad = power * joustbonus;}
+		qad = Random.Range (power * joustbonus, 2 * power * joustbonus);
+		if (Script3.monEnd >= qad) {qad = 0;}
+		else {qad -= Script3.monEnd;}
 		if (Script3.monHP > qad){Script3.monHP = Script3.monHP - qad;}
 		else {experience += Script3.monExp; Destroy(stabVictim); killCount++;}
+		if (qad > 0) 
+		{
+			Instantiate(damageEffect, new Vector3(
+				(stabVictim.transform.position.x + player.transform.position.x) / 2 * 0.95f, 5f, 
+				(stabVictim.transform.position.z + player.transform.position.z) / 2 * 0.95f), 
+			            Quaternion.Euler(90, 330, 0));
+		}
 		// add level up flag here
 		joustbonus = 0;
 		conflict = false;
 		countdown = 0f;
 		if (movement <= 0) {MonSpawn ();}
+
 	}
 
 	void MonSpawn () {
@@ -180,15 +215,15 @@ public class GCScript : MonoBehaviour {
 
 		if (experience >= (2*level+1)*10) 
 		{
-			power++;
+			power += 5;
 			speed++;
-			endurance++;
-			playHP += 10;
+			endurance += 5;
+			playHP += Random.Range (5, 16) * 10;
 			experience -= (2*level+1)*10;
 			level++;
 		}
 		spawnCount = 0;
-
+		monCount = 0;
 	redux:
 		ranSide = Random.Range (0, 3);
 		ranSquareX = Random.Range (1, mapX*2 - 1);
@@ -201,6 +236,7 @@ public class GCScript : MonoBehaviour {
 		if (newMonX == pLocX && newMonZ == pLocZ) {goto redux;} // do not spawn on player
 		foreach(GameObject enemy in GameObject.FindGameObjectsWithTag("foe")) // do not spawn on other enemy
 		{
+			monCount++;
 			MonControl Script1 = enemy.GetComponent<MonControl>();
 			if (Script1.monX == newMonX && Script1.monZ == newMonZ){goto redux;}	
 		}
@@ -209,7 +245,7 @@ public class GCScript : MonoBehaviour {
 		if (70 < ranType && ranType < 120) {newMonType = krusher;}
 		
 
-		Instantiate(newMonType, new Vector3(newMonX*5f, 2.5f, newMonZ*5f), Quaternion.identity);
+		if (monCount < 0) {Instantiate(newMonType, new Vector3(newMonX*5f, 2.5f, newMonZ*5f), Quaternion.identity);}
 
 		if (spawnCount < level) {spawnCount++; goto redux;}
 
